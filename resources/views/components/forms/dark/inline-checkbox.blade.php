b671cf13bc408eee66c32336d5917714c51106f0
Fariz Huzairi <farizhuzairi@gmail.com>
2024-07-23 22:51:05 +0700

    update elemen form

--- resources/views/components/forms/dark/inline-checkbox.blade.php
+++ resources/views/components/forms/dark/inline-checkbox.blade.php
@@ -10,12 +10,12 @@
     ])
 }}>
     <input type="checkbox" {{
-        $input->attributes->merge(['class' => $attributes->prepends('appearance-none checked:text-primary focus:text-primary border-0 border-slate-600 active:border-slate-600 outline-none active:outline-none focus:outline-none ring-0 active:ring-0 focus:ring-0 bg-slate-400 active:bg-slate-400 focus:bg-slate-400 rounded')])
+        $input->attributes->merge(['class' => $attributes->prepends('appearance-none checked:text-amber-800 focus:text-amber-800 border-0 border-slate-600 active:border-slate-600 outline-none active:outline-none focus:outline-none ring-0 active:ring-0 focus:ring-0 bg-slate-400 active:bg-slate-400 focus:bg-slate-400 rounded')])
         ->merge([
             'name' => '',
             'id' => '',
             'disabled' => $isDisabled
         ])
     }}>
-    <span class="text-slate-300">{{ $labelText }}</span>
+    <span>{{ $labelText }}</span>
 </label>
